bb34e7ef31b9ccb536728cf2cae7981df8a07d04
PEARHJ <0425qoguswls@naver.com>
2023-11-02 15:24:48 +0900

    좋아요 수정

--- app/src/main/java/com/cchaegog/chaegog/Model/FeedFavorite.java
+++ app/src/main/java/com/cchaegog/chaegog/Model/FeedFavorite.java
@@ -4,41 +4,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FeedFavorite {
-    public int favoriteCount;
-    public Map<String, Boolean> favorites = new HashMap<>();
-//    private ArrayList<String> favoriteUserList;
 
-    public FeedFavorite(int favoriteCount, Map<String, Boolean> favorites) {
-        this.favoriteCount = favoriteCount;
-        this.favorites = favorites;
-//        this.favoriteUserList = favoriteUserList;
+    private String postId;
+
+    public FeedFavorite(String postId) {
+        this.postId = postId;
     }
 
-    public FeedFavorite() {
-
+    public String getPostId() {
+        return postId;
     }
 
-    public int getFavoriteCount() {
-        return favoriteCount;
+    public void setPostId(String postId) {
+        this.postId = postId;
     }
 
-    public void setFavoriteCount(int favoriteCount) {
-        this.favoriteCount = favoriteCount;
-    }
-
-    public Map<String, Boolean> getFavorites() {
-        return favorites;
-    }
-
-    public void setFavorites(Map<String, Boolean> favorites) {
-        this.favorites = favorites;
-    }
-
-    //    public ArrayList<String> getFavoriteUserList() {
-//        return favoriteUserList;
+    //    public int favoriteCount;
+//    public Map<String, Boolean> favorites = new HashMap<>();
+////    private ArrayList<String> favoriteUserList;
+//
+//    public FeedFavorite(int favoriteCount, Map<String, Boolean> favorites) {
+//        this.favoriteCount = favoriteCount;
+//        this.favorites = favorites;
+////        this.favoriteUserList = favoriteUserList;
 //    }
 //
-//    public void setFavoriteUserList(ArrayList<String> favoriteUserList) {
-//        this.favoriteUserList = favoriteUserList;
+//    public FeedFavorite() {
+//
 //    }
+//
+//    public int getFavoriteCount() {
+//        return favoriteCount;
+//    }
+//
+//    public void setFavoriteCount(int favoriteCount) {
+//        this.favoriteCount = favoriteCount;
+//    }
+//
+//    public Map<String, Boolean> getFavorites() {
+//        return favorites;
+//    }
+//
+//    public void setFavorites(Map<String, Boolean> favorites) {
+//        this.favorites = favorites;
+//    }
+
 }
